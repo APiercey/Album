2bb63e5da6acf2e9caa8397fb89ef2876893d76c
Charlton <sholxz@gmail.com>
2013-11-24 22:02:00 -0500

    Sholas commit function mysl escape

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
+function mysql_prep($string){
+	global $connection;
+	
+	$escaped_string = mysqli_real_escape_string($connection, $string);
+	return $escaped_string;
+	}
+
 function validateEmail($email)
 {
 	$validEmailExpr = "^[0-9a-z~!#$%&_-]([.]?[0-9a-z~!#$%&_-])*" .
