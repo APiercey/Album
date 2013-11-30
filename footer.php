cd6d245b189030457232116f76e0831e3d521991
APiercey <alexander.piercey@gmail.com>
2013-11-30 16:59:11 -0500

    style

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
 	</div>
+	<footer>
+		Produced by Alexander Butt-Piercey &amp; Shola Olaniyan of the Internet Applications and Web Development program at the Algonquin College.
+	</footer>
 </body>
 </html>
